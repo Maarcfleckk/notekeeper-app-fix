26621f183e72b3d3c2cc8468d85125d0950694a3
Marc Fleck <marcfleckrossell4@gmail.com>
2024-06-09 20:40:54 +0200

    fix small error

--- src/components/UpdateNoteForm.jsx
+++ src/components/UpdateNoteForm.jsx
@@ -7,7 +7,7 @@ export const UpdateNoteForm = ({
   handleUpdatingNoteChange,
 }) => {
   const formatDateInput = (date) => {
-    const [day, month, year] = date.split("/");
+    const [month, day, year] = date.split("/");
     const formattedDate = `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
     return formattedDate;
   };
